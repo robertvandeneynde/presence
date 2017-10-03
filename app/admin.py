81b69c62ff6a2f0edd54c479ebdd31efff77ce29
Robert Vanden Eynde <robertvandeneynde@hotmail.com>
2017-10-03 10:44:31 +0200

    auto

--- app/admin.py
+++ app/admin.py
@@ -10,14 +10,14 @@ from .models import *
 
 def add(cls, *disp, **kwargs):
     class Admin(admin.ModelAdmin):
-        list_display = disp # tuple(f.name for f in cls._meta.get_fields())
+        list_display = tuple(d[1:] if d.startswith('-') else d for d in disp) # tuple(f.name for f in cls._meta.get_fields())
         ordering = disp
         # filter_horizontal = ('presents', )
         # formfield_overrides = { models.ManyToManyField: {'widget': CheckboxSelectMultiple}, }
 
     admin.site.register(cls, Admin)
 
-add(Group, 'year', 'name')
+add(Group, '-year', 'name')
 add(Session, 'group', 'beg', 'end')
 add(Student, 'first_name', 'last_name', 'group', 'classe')
 # add(Presence, 'student', 'session')
